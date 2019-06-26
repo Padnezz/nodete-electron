22886fe00bfd21efc5706d9a15c7abf790e32735
André Pettersson <padnezz@gmail.com>
2019-06-26 13:56:34 +0200

    gonna reinstall computer brb

--- js/script.js
+++ js/script.js
@@ -1,46 +1,72 @@
 const { ipcRenderer } = require('electron');
+var snmp = require("net-snmp");
 var cy;
-function updateInTraffic(node, interface, traffic) {
-
+function updateInTraffic(hostname, interface, traffic) {
+  interface = interface.replace("/", "--");
+  ipcRenderer.send('getLink', '{"status":"REQ", "interface":"' + interface + '", "hostname":"' + hostname + '", "traffic":"' + traffic + '"}');
 }
 
-function addNode(ip, hostname){
-  ipcRenderer.send('addNode', '{"status":"REQ", "ip":"'+ip+'", "hostname":"'+hostname+'"}');
-  cy.add({group: 'nodes', data: {id:hostname}, position: {x:100, y:300}});
+ipcRenderer.on('getLink', (event, arg) => {
+  var parseArg = JSON.parse(arg);
+  if (parseArg.status == "ACK") {
+    var session = snmp.createSession("127.0.0.1", "public");
+
+    var oids = ["1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0"];
+
+    session.get(oids, function (error, varbinds) {
+      if (error) {
+        console.error(error);
+      } else {
+        for (var i = 0; i < varbinds.length; i++)
+          if (snmp.isVarbindError(varbinds[i]))
+            console.error(snmp.varbindError(varbinds[i]))
+          else
+            console.log(varbinds[i].oid + " = " + varbinds[i].value);
+      }
+
+      session.close();
+    });
+    cy.style().selector("#" + parseArg.id).style(parseArg.st + "-label", parseArg.traffic + "Mbit/s").update();
+  }
+});
+
+function addNode(ip, hostname) {
+  ipcRenderer.send('addNode', '{"status":"REQ", "ip":"' + ip + '", "hostname":"' + hostname + '"}');
+  cy.add({ group: 'nodes', data: { id: hostname }, position: { x: 100, y: 300 } });
   cy.style().selector("#" + hostname).style('label', hostname).update();
   saveVisual();
 }
 
-function addLink(source, target, source_int, target_int){
+function addLink(source, target, source_int, target_int) {
   source_int = source_int.replace("/", "--");
   target_int = target_int.replace("/", "--");
-  ipcRenderer.send('addLink', '{"status":"REQ", "source":"'+source+'", "target":"'+target+'", "source_int":"'+source_int+'", "target_int":"'+target_int+'"}');
+  ipcRenderer.send('addLink', '{"status":"REQ", "source":"' + source + '", "target":"' + target + '", "source_int":"' + source_int + '", "target_int":"' + target_int + '"}');
   var id = source + "-" + source_int + "_" + target + "-" + target_int;
-  cy.add({group: 'edges', data: { id: id, source: source, target: target }});
+  cy.add({ group: 'edges', data: { id: id, source: source, target: target } });
   cy.style().selector("#" + id).style('line-color', "green").style("source-label", "0Mbit/s").style("target-label", "0Mbit/s").style("source-text-offset", "47px").style("target-text-offset", "47px").style("text-background-color", "black").style("text-background-opacity", "1").style("text-background-padding", "1px").style("color", "white").update();
   saveVisual();
 }
 
-function deleteNode(hostname){
+function deleteNode(hostname) {
   //CheckLinks
-  ipcRenderer.send('deleteNode', '{"status":"REQ", "hostname":"'+hostname+'"}');
-  cy.remove("#"+hostname);
+  ipcRenderer.send('deleteNode', '{"status":"REQ", "hostname":"' + hostname + '"}');
+  cy.remove("#" + hostname);
   saveVisual();
 }
 
-function deleteLink(hostname, interface){
+function deleteLink(hostname, interface) {
   interface = interface.replace("/", "--");
-  ipcRenderer.send('deleteLink', '{"status":"REQ", "hostname":"'+hostname+'", "interface":"'+interface+'"}');
+  ipcRenderer.send('deleteLink', '{"status":"REQ", "hostname":"' + hostname + '", "interface":"' + interface + '"}');
 }
 
-function saveVisual(){
-  ipcRenderer.send('saveVisual', '{"status":"REQ", "cyjson":'+JSON.stringify(cy.json())+'}');
+function saveVisual() {
+  ipcRenderer.send('saveVisual', '{"status":"REQ", "cyjson":' + JSON.stringify(cy.json()) + '}');
 }
 
 ipcRenderer.on('startVisual', (event, arg) => {
   var parseArg = JSON.parse(arg);
   console.log(arg);
-  if(parseArg.status == "ACK"){
+  if (parseArg.status == "ACK") {
     /*parseArg.rows.forEach((row) => {
       console.log(row.hostname);
       cy.add({group: 'nodes', data: {id:row.hostname}, position: {x:100, y:300}});
@@ -52,8 +78,8 @@ ipcRenderer.on('startVisual', (event, arg) => {
 
 ipcRenderer.on('deleteLink', (event, arg) => {
   var parseArg = JSON.parse(arg);
-  if(parseArg.status == "ACK"){
-    cy.remove("#"+parseArg.id);
+  if (parseArg.status == "ACK") {
+    cy.remove("#" + parseArg.id);
     saveVisual();
   }
 });
--- js/sqlite.js
+++ js/sqlite.js
@@ -4,95 +4,102 @@ const { ipcMain } = require('electron');
 let db;
 sqlite3.verbose()
 db = new sqlite3.Database('./db/nodete.db', (err) => {
-    if (err) {
-        console.error(err.message); 
-    }
+  if (err) {
+    console.error(err.message);
+  }
 });
 
-function getLink(hostname, interface){
-    db.get(`SELECT * FROM links WHERE source = ? AND source_int = ?`, [hostname, interface], function(err, rows) {
-        if (err) {
-          console.error(err.message);
-        } else {
-          return JSON.stringify(rows);
-        }
-    });
-    db.get(`SELECT * FROM links WHERE target = ? AND target_int = ?`, [hostname, interface], function(err, rows) {
-        if (err) {
-          console.error(err.message);
-        } else {
-          return JSON.stringify(rows);
-        }
-    });
-}
-
 ipcMain.on('addNode', (event, arg) => {
-    var parseArg = JSON.parse(arg);
-    if (parseArg.status == "REQ") {
-        db.run(`INSERT INTO nodes(ip, hostname) VALUES(?, ?)`, [parseArg.ip, parseArg.hostname], function(err) {
-            if (err) {
-              console.error(err.message);
-            }
-        });
-    }
+  var parseArg = JSON.parse(arg);
+  if (parseArg.status == "REQ") {
+    db.run(`INSERT INTO nodes(ip, hostname) VALUES(?, ?)`, [parseArg.ip, parseArg.hostname], function (err) {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+  }
 });
 
 ipcMain.on('deleteNode', (event, arg) => {
-    var parseArg = JSON.parse(arg);
-    if (parseArg.status == "REQ") {
-        db.run(`DELETE FROM nodes WHERE hostname = ?`, [parseArg.hostname], function(err) {
-            if (err) {
-              console.error(err.message);
-            }
-        });
-    }
+  var parseArg = JSON.parse(arg);
+  if (parseArg.status == "REQ") {
+    db.run(`DELETE FROM nodes WHERE hostname = ?`, [parseArg.hostname], function (err) {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+  }
 });
 
 ipcMain.on('addLink', (event, arg) => {
-    var parseArg = JSON.parse(arg);
-    if (parseArg.status == "REQ") {
-        db.run(`INSERT INTO links(source, target, source_int, target_int) VALUES(?, ?, ?, ?)`, [parseArg.source, parseArg.target, parseArg.source_int, parseArg.target_int], function(err) {
-            if (err) {
-              console.error(err.message);
-            }
-        });
-    }
+  var parseArg = JSON.parse(arg);
+  if (parseArg.status == "REQ") {
+    db.run(`INSERT INTO links(source, target, source_int, target_int) VALUES(?, ?, ?, ?)`, [parseArg.source, parseArg.target, parseArg.source_int, parseArg.target_int], function (err) {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+  }
 });
 
 ipcMain.on('deleteLink', (event, arg) => {
-    var parseArg = JSON.parse(arg);
-    if (parseArg.status == "REQ") {
-        db.get(`SELECT * FROM links WHERE source = ? AND source_int = ?`, [parseArg.hostname, parseArg.interface], function(err, rows) {
+  var parseArg = JSON.parse(arg);
+  if (parseArg.status == "REQ") {
+    db.get(`SELECT * FROM links WHERE source = ? AND source_int = ?`, [parseArg.hostname, parseArg.interface], function (err, rows) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        if (rows != undefined) {
+          delete_row = rows.id;
+          db.run(`DELETE FROM links WHERE source = ? AND source_int = ?`, [rows.source, rows.source_int], function (err) {
             if (err) {
               console.error(err.message);
             } else {
-                if(rows != undefined){
-                    delete_row = rows.id;
-                    db.run(`DELETE FROM links WHERE source = ? AND source_int = ?`, [rows.source, rows.source_int], function(err) {
-                        if (err) {
-                          console.error(err.message);
-                        } else {
-                          event.sender.send('deleteLink', '{"status":"ACK", "id":"' + rows.source + "-" + rows.source_int + "_" + rows.target + "-" + rows.target_int + '"}');
-                        }
-                    });
-                }
+              event.sender.send('deleteLink', '{"status":"ACK", "id":"' + rows.source + "-" + rows.source_int + "_" + rows.target + "-" + rows.target_int + '"}');
             }
-        });
-        db.get(`SELECT * FROM links WHERE target = ? AND target_int = ?`, [parseArg.hostname, parseArg.interface], function(err, rows) {
+          });
+        }
+      }
+    });
+    db.get(`SELECT * FROM links WHERE target = ? AND target_int = ?`, [parseArg.hostname, parseArg.interface], function (err, rows) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        if (rows != undefined) {
+          delete_row = rows.id;
+          db.run(`DELETE FROM links WHERE target = ? AND target_int = ?`, [rows.target, rows.target_int], function (err) {
             if (err) {
               console.error(err.message);
             } else {
-                if(rows != undefined){
-                    delete_row = rows.id;
-                    db.run(`DELETE FROM links WHERE target = ? AND target_int = ?`, [rows.target, rows.target_int], function(err) {
-                        if (err) {
-                          console.error(err.message);
-                        } else {
-                          event.sender.send('deleteLink', '{"status":"ACK", "id":"' + rows.source + "-" + rows.source_int + "_" + rows.target + "-" + rows.target_int + '"}');
-                        }
-                    });
-                }
+              event.sender.send('deleteLink', '{"status":"ACK", "id":"' + rows.source + "-" + rows.source_int + "_" + rows.target + "-" + rows.target_int + '"}');
             }
-        });
-    }
+          });
+        }
+      }
+    });
+  }
+});
+
+ipcMain.on('getLink', (event, arg) => {
+  var parseArg = JSON.parse(arg);
+  if (parseArg.status == "REQ") {
+    db.get(`SELECT * FROM links WHERE source = ? AND source_int = ?`, [parseArg.hostname, parseArg.interface], function (err, rows) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        if (rows != undefined) {
+        event.sender.send('getLink', '{"status":"ACK", "st":"source", "traffic":"'+parseArg.traffic+'", "hostname":"'+parseArg.hostname+'", "interface":"'+parseArg.interface+'", "id":"' + rows.source + "-" + rows.source_int + "_" + rows.target + "-" + rows.target_int + '"}');
+        }
+      }
+    });
+    db.get(`SELECT * FROM links WHERE target = ? AND target_int = ?`, [parseArg.hostname, parseArg.interface], function (err, rows) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        if (rows != undefined) {
+        event.sender.send('getLink', '{"status":"ACK", "st":"target", "traffic":"'+parseArg.traffic+'", "hostname":"'+parseArg.hostname+'", "interface":"'+parseArg.interface+'", "id":"' + rows.source + "-" + rows.source_int + "_" + rows.target + "-" + rows.target_int + '"}');
+        }  
+      }
+    });
+  }
 });
